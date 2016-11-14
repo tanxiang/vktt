d94eb2f758d72756861446387aed47bd0c4d24fb
Tan Xiang <tanxiang>
2016-11-14 17:03:10 +0800

    name ch

---
+++ fmap.cc
@@ -0,0 +1,3 @@
+#include "fmap.hh"
+#include <sys/mman.h>
+
---
+++ fmap.hh
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <experimental/filesystem>
+
+namespace tt{
+class fileMapBuf{
+};
+}//namespace
