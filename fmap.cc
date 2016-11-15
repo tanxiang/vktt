a3fa182ae4c6305d1ba52b65f0464940f8a5ea9d
Tan Xiang <tanxiang>
2016-11-15 16:20:31 +0800

    name ch

--- fmap.cc
+++ fmap.cc
@@ -7,13 +7,13 @@
 
 namespace tt{
 
-std::unique_ptr<void,std::function<void(void*)>> fileMapBuf(const char* fileName,size_t* fileSize)
+FileMap fileMapBuf(const char* fileName,size_t* fileSize)
 {
 	auto fd = open(fileName, O_RDONLY);
 	struct stat fileStat;
 	fstat(fd, &fileStat);
 	if(fileSize) *fileSize = fileStat.st_size;
-	auto buf = std::unique_ptr<void,std::function<void(void*)>>{
+	auto buf = FileMap{
 		mmap(NULL, fileStat.st_size, PROT_READ, MAP_SHARED, fd, 0),
 		[=](void* p){
 			munmap(p,fileStat.st_size);
--- fmap.hh
+++ fmap.hh
@@ -1,11 +1,10 @@
 #pragma once
 #include <iostream>
 #include <experimental/filesystem>
-
 #include <memory>
 
-
 namespace tt{
-std::unique_ptr<void,std::function<void(void*)>> fileMapBuf(const char* fileName,size_t* fileSize = nullptr);
+using FileMap = std::unique_ptr<void,std::function<void(void*)>>;
+FileMap fileMapBuf(const char* fileName,size_t* fileSize = nullptr);
 
 }//namespace
